d496f094a4d57e67f60107b6536c1c713c590d7f
DasLixou <einlixou@gmail.com>
2024-10-10 21:29:35 +0200

    Use iterators instead

--- src/peaks.rs
+++ src/peaks.rs
@@ -1,49 +1,62 @@
-#[derive(Debug, PartialEq, Clone, Copy)]
-pub struct Peak {
-    pub min: f32,
-    pub max: f32,
+#[derive(Default, Debug, PartialEq, Clone, Copy)]
+pub struct Peak<T> {
+    pub min: T,
+    pub max: T,
 }
 
-/// Detects peaks in chunks of size `chunk_size` in the data.
-///
-/// `chunk_size` must be 2 or any power of two above.
-///
-/// If `chunk_size` does not divide the length of the
-/// slice, then the last peak will only work over the remaining data.
-pub fn peaks_f32(data: &[f32], chunk_size: usize) -> Vec<Peak> {
-    debug_assert!(chunk_size >= 2);
-    debug_assert!(chunk_size.is_power_of_two());
+pub trait SlicePeakExt {
+    type Item;
 
-    let mut peaks = Vec::with_capacity(data.len().div_ceil(chunk_size));
+    fn peak(self) -> Peak<Self::Item>;
+}
 
-    for chunk in data.chunks(chunk_size) {
-        let peak = chunk
-            .iter()
-            .fold(Peak { min: 0., max: 0. }, |peak, sample| Peak {
-                min: sample.min(peak.min),
-                max: sample.max(peak.max),
-            });
-        peaks.push(peak);
+impl<T> SlicePeakExt for &[T]
+where
+    T: Peakable,
+{
+    type Item = T;
+
+    /// Returns the highest and lowest elements in this slice according to [`Peakable`]
+    ///
+    /// # Example
+    ///
+    /// ```ignore
+    /// samples.chunks(4).map(SlicePeakExt::peak)
+    /// ```
+    fn peak(self) -> Peak<Self::Item> {
+        self.into_iter()
+            .fold(Peak::default(), |peak, &sample| Peak {
+                min: sample.lower(peak.min),
+                max: sample.higher(peak.max),
+            })
     }
+}
 
-    peaks
+pub trait Peakable: Default + Copy {
+    fn lower(self, other: Self) -> Self;
+    fn higher(self, other: Self) -> Self;
+}
+
+impl Peakable for f32 {
+    fn lower(self, other: Self) -> Self {
+        self.min(other)
+    }
+    fn higher(self, other: Self) -> Self {
+        self.max(other)
+    }
 }
 
 #[cfg(test)]
 mod test {
-    use crate::peaks::Peak;
-
-    use super::peaks_f32;
+    use crate::peaks::{Peak, SlicePeakExt};
 
     #[test]
     fn peaks_4() {
-        let peaks = peaks_f32(
-            &[
-                0.0, 1.4, -3.4, 2.1, // chunk 1
-                -1.3, 5.3, 2.1, 0.9, // chunk 2
-            ],
-            4,
-        );
+        let samples: &[f32] = &[
+            0.0, 1.4, -3.4, 2.1, // chunk 1
+            -1.3, 5.3, 2.1, 0.9, // chunk 2
+        ];
+        let peaks: Vec<_> = samples.chunks(4).map(SlicePeakExt::peak).collect();
 
         assert_eq!(
             peaks,
