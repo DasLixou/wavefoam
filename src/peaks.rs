ab314ae985be52f296e968964cd454a87ba0bd18
DasLixou <einlixou@gmail.com>
2024-10-11 20:57:07 +0200

    AVX2 acceleration

--- src/peaks.rs
+++ src/peaks.rs
@@ -1,3 +1,5 @@
+pub mod f32;
+
 #[derive(Default, Debug, PartialEq, Clone, Copy)]
 pub struct Peak<T> {
     pub min: T,
@@ -7,15 +9,6 @@ pub struct Peak<T> {
 pub trait SlicePeakExt {
     type Item;
 
-    fn peak(self) -> Peak<Self::Item>;
-}
-
-impl<T> SlicePeakExt for &[T]
-where
-    T: Peakable,
-{
-    type Item = T;
-
     /// Returns the highest and lowest elements in this slice according to [`Peakable`]
     ///
     /// # Example
@@ -23,27 +16,7 @@ where
     /// ```ignore
     /// samples.chunks(4).map(SlicePeakExt::peak)
     /// ```
-    fn peak(self) -> Peak<Self::Item> {
-        self.into_iter()
-            .fold(Peak::default(), |peak, &sample| Peak {
-                min: sample.lower(peak.min),
-                max: sample.higher(peak.max),
-            })
-    }
-}
-
-pub trait Peakable: Default + Copy {
-    fn lower(self, other: Self) -> Self;
-    fn higher(self, other: Self) -> Self;
-}
-
-impl Peakable for f32 {
-    fn lower(self, other: Self) -> Self {
-        self.min(other)
-    }
-    fn higher(self, other: Self) -> Self {
-        self.max(other)
-    }
+    fn peak(self) -> Peak<Self::Item>;
 }
 
 #[cfg(test)]
@@ -70,6 +43,23 @@ mod test {
                     max: 5.3
                 }
             ]
+        );
+    }
+
+    #[test]
+    fn peak_8() {
+        let samples: &[f32] = &[
+            0.0, 1.4, -3.4, 2.1, // chunk 1
+            -1.3, 5.3, 2.1, 0.9, // chunk 2
+        ];
+        let peak = SlicePeakExt::peak(samples);
+
+        assert_eq!(
+            peak,
+            Peak {
+                min: -3.4,
+                max: 5.3
+            }
         );
     }
 }
---
+++ src/peaks/f32.rs
@@ -0,0 +1,66 @@
+use std::mem::MaybeUninit;
+
+use super::{Peak, SlicePeakExt};
+
+impl SlicePeakExt for &[f32] {
+    type Item = f32;
+
+    fn peak(self) -> Peak<Self::Item> {
+        #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+        {
+            // TODO: actually benchmark this on large data
+            if is_x86_feature_detected!("avx2") {
+                #[cfg(target_arch = "x86")]
+                use std::arch::x86::*;
+                #[cfg(target_arch = "x86_64")]
+                use std::arch::x86_64::*;
+
+                let blocks = self.chunks_exact(8);
+                let remainder = blocks.remainder();
+
+                let mut min = unsafe { _mm256_setzero_ps() };
+                let mut max = unsafe { _mm256_setzero_ps() };
+
+                for block in blocks {
+                    let hiaddr = block.as_ptr();
+                    let loaddr = unsafe { hiaddr.offset(4) };
+                    let samples = unsafe { _mm256_loadu2_m128(hiaddr, loaddr) };
+                    min = unsafe { _mm256_min_ps(min, samples) };
+                    max = unsafe { _mm256_max_ps(max, samples) };
+                }
+
+                // TODO: validate that it works on both endians
+                let min = unsafe {
+                    let mut array: MaybeUninit<[f32; 8]> = MaybeUninit::uninit();
+                    _mm256_store_ps(array.as_mut_ptr().cast(), min);
+                    array.assume_init()
+                };
+                let max = unsafe {
+                    let mut array: MaybeUninit<[f32; 8]> = MaybeUninit::uninit();
+                    _mm256_store_ps(array.as_mut_ptr().cast(), max);
+                    array.assume_init()
+                };
+
+                return Peak {
+                    min: min
+                        .into_iter()
+                        .chain(remainder.into_iter().copied())
+                        .reduce(f32::min)
+                        .unwrap(),
+                    max: max
+                        .into_iter()
+                        .chain(remainder.into_iter().copied())
+                        .reduce(f32::max)
+                        .unwrap(),
+                };
+            }
+        }
+
+        // TODO: think about default - when it's between 2. and 5., min would still be 0 (and same for avx2 impl)
+        self.into_iter()
+            .fold(Peak::default(), |peak, &sample| Peak {
+                min: sample.min(peak.min),
+                max: sample.max(peak.max),
+            })
+    }
+}
